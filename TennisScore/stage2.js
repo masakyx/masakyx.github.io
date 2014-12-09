ed55d140e790fd94a8a2c8443b6c7c2f54e819b2
masaki <masaki@ubuntu.ubuntu-domain>
2014-12-09 11:28:49 +0900

    10commit

--- TennisScore/stage2.js
+++ TennisScore/stage2.js
@@ -1,19 +1,21 @@
 jQuery(function ($){
-  var i=0;
+  var i=0;//change1 number
+  var k=0;//change2 number
     $("#change1").click(function (){
       i++;
       console.log(i);
       if(i%2 == 1){
-        $(this).css("background","url(bolay.jpg) left top no-repeat");
+        $(this).css("background-image","url(bolay.jpg)");
       }else{
-        $(this).css("background","url(stroke.jpg) left top no-repeat");
+        $(this).css("background-image","url(stroke.jpg)");
       }
     });
-
-    $("#returnin").click(function(){
-      $(this).slideUp();
-      });
-    $("#service").click(function (){
-      $(this).css("background-color","green");
-      });
-    });  
+    $("#change2").click(function (){
+      k++;
+      if(k%2 == 1){
+        $(this).css("background-image","url(bolay.jpg)");
+      }else{
+        $(this).css("background-image","url(stroke.jpg)");
+      }
+    });
+});
