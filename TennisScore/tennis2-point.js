bc475153447c32bbc735f8aacac59983e311ce10
masaki <masaki@ubuntu.ubuntu-domain>
2014-12-17 14:27:41 +0900

    add point-method 2

--- TennisScore/tennis2-point.js
+++ TennisScore/tennis2-point.js
@@ -1,30 +1,79 @@
 //変数定義
 var point1 = 0;
 var point2 = 0;
+var gamepoint1 = 0;
+var gamepoint2 = 0;
+var setpoint1 = 0;
+var setpoint2 = 0;
+
+//ID取得
+var score1 = $("#score1");
+var score2 = $("#score2");
+var gamest1 = $("#gamest1");
+var gamest2 = $("#gamest2");
+var setst1 = $("#setst1");
+var setst2 = $("#setst2");
 
 jQuery(function($){
-   //ID変数　取得
-   var score1 = $("#score1");
-   var score2 = $("#score2");
 
    $("#serviceace,#rm,#win1,#side2,#back2,#net2").click(function(){
      point1++;
      ScorePoint(score1,point1);
+     console.log("point1="+point1);
    });
    $("#returnace,#win2,#side1,#back1,#net1").click(function(){
      point2++;
      ScorePoint(score2,point2);
+     console.log("point2="+point2);
    });
 });
 
-//スコアーメソッド
+//スコアーメソッド  
 function ScorePoint(score,point){
      if(point == 1){
        score.text("15");
      }else if(point == 2){
        score.text("30");
-     }else if(point == 3){
-       score.text("40");
-     }
+     }else if(point == 3 && point1 < 3 || point == 3 && point2 < 3){
+       score.text("40"); 
+     }else if(point1 == 3 && point2 == 3){
+       score1.text("DEUCE");
+       score2.text("DEUCE");
+     }else if(point1 == 3 && point2 == 4 || point1 == 4 && point2 == 3){
+       if(point1 > point2){
+         score1.text("Ad");
+       }else if(point2 > point1){
+         score2.text("Ad");
+       }
+     }else if(point1 == 4 && point2 == 4){
+       point1 = 3; point2 = 3;
+       score1.text("DEUCE"); score2.text("DEUCE");
+     }else if(point1 == 3 && point2 == 5 || point1 == 5 && point2 == 3){
+       if(point1 > point2){
+         gamepoint1++;
+         GamePoint(gamest1,gamepoint1);
+       }else if(point2 > point1){
+         gamepoint2++;
+         GamePoint(gamest2,gamepoint2);
+       }
+     }else if(point1 == 4 && point2 < 3){
+       gamepoint1++;
+       GamePoint(gamest1,gamepoint1);
+     }else if(point2 == 4 && point1 < 3){
+       gamepoint2++;
+       GamePoint(gamest2,gamepoint2);
+     }                        
      return 0;
+}
+
+function GamePoint(gamest,gamepoint){
+  if(gamepoint < 6){
+    point1 = 0;
+    point2 = 0;
+    score1.text("0");
+    score2.text("0");
+    gamest.text(gamepoint);
+  }
+
+  return 0;
 }
