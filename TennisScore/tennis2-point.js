3dba49019adf96e03066802f9325652c2e780c93
masaki <masaki@ubuntu.ubuntu-domain>
2014-12-17 15:48:42 +0900

    add method

--- TennisScore/tennis2-point.js
+++ TennisScore/tennis2-point.js
@@ -5,6 +5,7 @@ var gamepoint1 = 0;
 var gamepoint2 = 0;
 var setpoint1 = 0;
 var setpoint2 = 0;
+var setcount = 1;
 
 //ID取得
 var score1 = $("#score1");
@@ -13,10 +14,27 @@ var gamest1 = $("#gamest1");
 var gamest2 = $("#gamest2");
 var setst1 = $("#setst1");
 var setst2 = $("#setst2");
+var service = $("#service");
+var retrunin = $("#returnin");
+var serviceace = $("#serviceace");
+var returnace = $("#returnace");
+var fault = $("#fault");
+var rm = $("#rm");
+var win1 = $("#win1");
+var win2 = $("#win2");
+var side1 = $("#side1");
+var side2 = $("#side2");
+var back1 = $("#back1");
+var back2 = $("#back2");
+var net1 = $("#net1");
+var net2 = $("#net2");
+var set1 = $("#set1");
+var set3 = $("#set3");
+var set5 = $("#set5");
 
 jQuery(function($){
 
-   $("#serviceace,#rm,#win1,#side2,#back2,#net2").click(function(){
+   $("#serviceace,#win1,#side2,#back2,#net2").click(function(){
      point1++;
      ScorePoint(score1,point1);
      console.log("point1="+point1);
@@ -25,6 +43,12 @@ jQuery(function($){
      point2++;
      ScorePoint(score2,point2);
      console.log("point2="+point2);
+   });
+   fault.click(function(){   
+     if(fault.val() == "Fault" ){
+      point2++;
+      ScorePoint(score2,point2);
+     }
    });
 });
 
@@ -66,14 +90,57 @@ function ScorePoint(score,point){
      return 0;
 }
 
+//ゲームカウントメソッド
 function GamePoint(gamest,gamepoint){
   if(gamepoint < 6){
     point1 = 0;
     point2 = 0;
     score1.text("0");
     score2.text("0");
-    gamest.text(gamepoint);
+    gamest.text(gamepoint);                  
+  }else if(gamepoint == 6 && gamepoint1 < 5 || gamepoint == 6 && gamepoint2 <5){
+    if(gamepoint1 > gamepoint2){
+      setpoint1++;
+      SetPoint(setst1,setpoint1);
+    }else if(gamepoint1 < gamepoint2){
+      setpoint2++;
+      SetPoint(setst2,setpoint2);
+    }
+  }
+  return 0;
+}
+
+//セットカウントメソッド
+function SetPoint(setst,setpoint){
+//セットカウントの取得
+  if(set1.is(":checked")){
+    setcount = 1;
+  }else if(set3.is(":checked")){
+    setcount = 3;
+  }else if(set5.is(":checked")){
+    setcount = 5;
+  }
+//セットポイントのカウント
+  point1=0;
+  point2=0;
+  score1.text("0");
+  score2.text("0");
+  gamepoint1=0;
+  gamepoint2=0;
+  gamest1.text("0");
+  gamest2.text("0");
+  setst.text(setpoint);
+  if(setcount == 1){
+
+  }else if(setcount = 3){
+
+  }else if(setcount = 3){
+
   }
 
-  return 0;
+}
+
+//タイブレイクメソッド
+function TieBreak(){
+
 }
