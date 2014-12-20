59929e54aa452edb087adcc3988bc43757b12aa3
masaki <masaki@ubuntu.ubuntu-domain>
2014-12-20 18:23:23 +0900

    Add new Class

---
+++ TenisScore-web.ver/pointcount.js
@@ -0,0 +1,14 @@
+//Player Object--------------------------------------------------------------------------
+function Player(){
+  var point=0,win=0,side=0,back=0,net=0,serviceace=0,fault=0,doublefault=0,returnace=0,returnmiss=0;
+}
+//Player1-------------------------------------------------------------------
+var p1 = new Player();
+var p2 = new Player();
+var p3 = new Player();
+var p4 = new Player();
+//--------------------------------------------------------------------------
+jQuery(function($){
+
+
+});
